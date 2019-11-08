659fc5f55becf0924b2c7bc93fcb46aff34fa751
Mylhow <dgascon@student.le-101.fr>
2019-11-08 19:43:46 +0100

    Fonction ft_free, boucle principal basique, etat

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,15 @@
 #include <get_next_line.h>
 
+#define Error -1
+#define Success 1
+#define EndOfFile 0
+
+int ft_free(void *ptr, int code)
+{
+    free(ptr);
+    return (code);
+}
+
 int get_next_line(int fd, char **line)
 {
     static char *buffer = NULL;
@@ -7,4 +17,16 @@ int get_next_line(int fd, char **line)
     char        *newBuffer;
     size_t      readSize;
     int         indexCharset;
+
+    readSize = 1;
+    while (readSize)
+    {
+        if (!(str = ft_calloc(BUFFER_SIZE + 1, sizeof(char))))
+            return (Error);
+        if ((readSize = read(fd, str, BUFFER_SIZE)) == -1)
+            return(ft_free(str, Error));
+        if (!(buffer = ft_strjoin(buffer, str)))
+            return (ft_free(str, Error));
+        free(str);
+    }
 }
