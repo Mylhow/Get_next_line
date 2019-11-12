1776fdc2bf8f593fcf75d408c64cb90eb4a4c2a3
Mylhow <dgascon@student.le-101.fr>
2019-11-12 16:00:04 +0100

    Norminette

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,18 @@
 /*   By: dgascon <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/12 15:28:45 by dgascon      #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/12 15:33:27 by dgascon     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/12 15:41:49 by dgascon     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-#define Error -1
-#define Success 1
-#define EndOfFile 0
+#define ERROR -1
+#define SUCCESS 1
+#define ENDFILE 0
 
-int ft_strchr(const char *str, int charset)
+int	ft_strchr(const char *str, int charset)
 {
 	int i;
 
@@ -32,7 +32,7 @@ int ft_strchr(const char *str, int charset)
 	return (0);
 }
 
-int ft_free(void *ptr, int code)
+int	ft_free(void *ptr, int code)
 {
 	free(ptr);
 	return (code);
@@ -40,53 +40,51 @@ int ft_free(void *ptr, int code)
 
 int	gnl_check(int fd, char **buffer)
 {
-	char        *str;
-	size_t      readSize;
-	int			state;	
-	char		*buffer_save;
+	char	*str;
+	size_t	read_size;
+	int		state;
+	char	*buffer_save;
 
 	state = 1;
-	if (!(str = malloc((BUFFER_SIZE + 1) *sizeof(char))))
-		return (Error);
-	if ((readSize = read(fd, str, BUFFER_SIZE)) == -1)
-		return(ft_free(str, Error));
-	str[readSize] = '\0';
+	if (!(str = malloc((BUFFER_SIZE + 1) * sizeof(char))))
+		return (ERROR);
+	if ((read_size = read(fd, str, BUFFER_SIZE)) == -1)
+		return (ft_free(str, ERROR));
+	str[read_size] = '\0';
 	(!ft_strlen(str)) ? state = 0 : 0;
-	buffer_save = *buffer;		
+	buffer_save = *buffer;
 	if (!(*buffer = ft_strjoin(*buffer, str)))
-		return (ft_free(str, Error));
+		return (ft_free(str, ERROR));
 	free(buffer_save);
 	free(str);
 	return (state);
 }
 
-int get_next_line(int fd, char **line)
+int	get_next_line(int fd, char **line)
 {
 	static char *buffer = NULL;
-	char        *newBuffer;
-	int         indexCharset;
-	int			stateGNLCheck;
+	char		*new_buffer;
+	int			index_charset;
+	int			state_gnlcheck;
 
 	(!buffer) ? buffer = ft_strdup("") : 0;
-	while (!(indexCharset = ft_strchr(buffer, '\n')))
+	while (!(index_charset = ft_strchr(buffer, '\n')))
 	{
-		stateGNLCheck = gnl_check(fd, &buffer);
-		if (stateGNLCheck == -1)
+		state_gnlcheck = gnl_check(fd, &buffer);
+		if (state_gnlcheck == -1)
 			return (-1);
-		else if (stateGNLCheck == 0)
+		else if (state_gnlcheck == 0)
 			break ;
 	}
-	if (indexCharset != 0)
+	new_buffer = NULL;
+	if (index_charset != 0)
 	{
-		*line = ft_substr(buffer, 0, indexCharset - 1);
-		newBuffer = ft_substr(buffer, indexCharset, ft_strlen(buffer));
+		*line = ft_substr(buffer, 0, index_charset - 1);
+		new_buffer = ft_substr(buffer, index_charset, ft_strlen(buffer));
 	}
 	else
-	{
-		newBuffer = NULL;
 		*line = ft_strdup(buffer);
-	}
 	free(buffer);
-	buffer = newBuffer;
-	return (indexCharset == 0) ? 0 : 1;
+	buffer = new_buffer;
+	return (index_charset == 0) ? ENDFILE : SUCCESS;
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,15 +5,15 @@
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: dgascon <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/11/12 10:49:47 by dgascon      #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/12 10:49:47 by dgascon     ###    #+. /#+    ###.fr     */
+/*   Created: 2019/11/12 15:56:28 by dgascon      #+#   ##    ##    #+#       */
+/*   Updated: 2019/11/12 15:59:42 by dgascon     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t  ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
 	size_t i;
 
@@ -23,61 +23,62 @@ size_t  ft_strlen(const char *s)
 	return (i);
 }
 
-char    *ft_strdup(const char *str)
+char	*ft_strdup(const char *str)
 {
-	char    *ptr;
-	int     strSize;
-	int     i;
+	char	*ptr;
+	int		str_size;
+	int		i;
 
-	strSize = ft_strlen(str);
-	if (!(ptr = malloc((strSize + 1) * sizeof(char))))
+	str_size = ft_strlen(str);
+	if (!(ptr = malloc((str_size + 1) * sizeof(char))))
 		return (NULL);
 	i = -1;
-	while (++i < strSize)
+	while (++i < str_size)
 		ptr[i] = str[i];
 	ptr[i] = '\0';
 	return (ptr);
 }
 
-char    *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-		char    *substr;
-		size_t  i;
-		size_t  maxsize;
+	char	*substr;
+	size_t	i;
+	size_t	maxsize;
 
-		i = -1;
-		if (s == 0)
-			return (NULL);
-		maxsize = (start >= ft_strlen(s)) ? 0 : len;
-		if (!(substr = malloc((maxsize + 1) * sizeof(char))))
-			return (NULL);
-		while (++i < maxsize && s[start + i])
-			substr[i] = s[start + i];
-		substr[i] = '\0';
-		return (substr);
+	i = -1;
+	if (s == 0)
+		return (NULL);
+	maxsize = (start >= ft_strlen(s)) ? 0 : len;
+	if (!(substr = malloc((maxsize + 1) * sizeof(char))))
+		return (NULL);
+	while (++i < maxsize && s[start + i])
+		substr[i] = s[start + i];
+	substr[i] = '\0';
+	return (substr);
 }
 
-char    *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-		char    *ptr;
-		size_t  sizes1;
-		size_t  sizes2;
-		size_t  i;
+	char	*ptr;
+	size_t	sizes1;
+	size_t	sizes2;
+	size_t	i;
 
-		if (s1 == 0 && s2 != 0)
-				return (ft_strdup(s2));
-		if (s2 == 0 && s1 != 0)
-				return (ft_strdup(s1));
-		sizes1 = ft_strlen(s1);
-		sizes2 = ft_strlen(s2);
-		if ((s1 == 0 && s2 == 0) || !(ptr = malloc((sizes1 + sizes2 + 1) * sizeof(char))))
-			return (NULL);
-		i = -1;
-		while (++i < sizes1)
-			ptr[i] = s1[i];
-		i = -1;
-		while (++i < sizes2)
-			ptr[i + sizes1] = s2[i];
-		ptr[sizes2 + sizes1] = '\0';
-		return (ptr);
+	if (s1 == 0 && s2 != 0)
+		return (ft_strdup(s2));
+	if (s2 == 0 && s1 != 0)
+		return (ft_strdup(s1));
+	sizes1 = ft_strlen(s1);
+	sizes2 = ft_strlen(s2);
+	if ((s1 == 0 && s2 == 0) ||
+		!(ptr = malloc((sizes1 + sizes2 + 1) * sizeof(char))))
+		return (NULL);
+	i = -1;
+	while (++i < sizes1)
+		ptr[i] = s1[i];
+	i = -1;
+	while (++i < sizes2)
+		ptr[i + sizes1] = s2[i];
+	ptr[sizes2 + sizes1] = '\0';
+	return (ptr);
 }
